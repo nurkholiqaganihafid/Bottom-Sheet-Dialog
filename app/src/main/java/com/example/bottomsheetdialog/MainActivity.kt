d92b1abda531b9b96fa140870eabb3c15c4df9ec
Nurkholiq Agani Hafid <nurkholiq.agani.hafid@gmail.com>
2021-09-08 16:43:03 +0700

    Update Project Bottom Sheet Dialog

--- app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.bottomsheetdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            val bottomSheetDialog = BottomSheetDialog(
+                this@MainActivity, R.style.BottomSheetDialogTheme
+            )
+
+            val bottomSheetView = LayoutInflater.from(applicationContext).inflate(
+                R.layout.layout_bottom_sheet, findViewById<LinearLayout>(R.id.bottomSheet)
+            )
+
+            bottomSheetView.findViewById<View>(R.id.buttonShare).setOnClickListener {
+                Toast.makeText(this@MainActivity, "Share...", Toast.LENGTH_SHORT).show()
+                bottomSheetDialog.dismiss()
+            }
+
+            bottomSheetDialog.setContentView(bottomSheetView)
+            bottomSheetDialog.show()
+
+        }
     }
 }
